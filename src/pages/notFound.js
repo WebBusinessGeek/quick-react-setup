6f4f93171ee43a4db9387c80c4372b4396c5f14c
Kevin Washington <kevw12188@gmail.com>
2015-11-18 10:31:14 -0500

    styled notFound page

--- src/pages/notFound.js
+++ src/pages/notFound.js
@@ -3,9 +3,12 @@ var React = require("react");
 NotFoundPage = React.createClass({
     render: function() {
         return(
-            <div id="NotFoundPageContainer">
-                <h4 id="NotFoundPageHeader">The page you are looking for doesn't exist.</h4>
-                <p id="NotFoundPageSubHeader">Go back <a href="/#/"> home </a> </p>
+            <div id="NotFoundPageContainer" className="text-center">
+                <div id="NotFoundPageHeader">
+                    <h1 id="NotFoundPageHeadline">Whoops! 404</h1>
+                    <h3 id="NotFoundPageSubHeadline">The page you are looking for doesn't exist.</h3>
+                </div>
+                <p id="NotFoundPageBackHome">Go back <a href="/#/"> home </a> </p>
             </div>
         )
     }
